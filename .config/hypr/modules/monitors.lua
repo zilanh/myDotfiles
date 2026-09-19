754b6cd047516dc86f3c19309807ff66f3c77264
Leo Collin <leo.collinh@gmail.com>
2026-09-19 19:56:13 +0000

    changed monitor settings

--- .config/hypr/modules/monitors.lua
+++ .config/hypr/modules/monitors.lua
@@ -4,28 +4,36 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-	output = "eDP-1",
-	mode = "1920x1200@60",
-	position = "0x0",
-	scale = 1,
+  output = "desc:AU Optronics 0xFA9B",
+  mode = "1920x1200@60",
+  position = "0x0",
+  scale = 1,
 })
 
 hl.monitor({
-	output = "",
-	mode = "1920x1200@60",
-	-- mode = "auto",
-	-- supports_hdr = 1,
-	mirror = "eDP-1",
-	position = "auto",
-	scale = 1,
+  output = "desc:LG Display 0x0590",
+  mode = "preferred",
+  position = "0x0",
+  scale = 1,
 })
+
 hl.monitor({
-	output = "desc:XGM XGIMI TV 0x00000001",
-	mode = "1920x1080@60",
-	supports_hdr = 1,
-	-- mirror = "eDP-1",
-	position = "auto",
-	scale = 1,
+  output = "desc:XGM XGIMI TV 0x00000001",
+  mode = "1920x1080@60",
+  supports_hdr = 1,
+  -- mirror = "eDP-1",
+  position = "auto",
+  scale = 1,
+})
+
+hl.monitor({
+  output = "",
+  mode = "preferred",
+  -- mode = "auto",
+  -- supports_hdr = 1,
+  mirror = "eDP-1",
+  position = "auto",
+  scale = 1,
 })
 
 -- hl.monitor({

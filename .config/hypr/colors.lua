da8ac73ad6b2aefc2ff8fa9312dbc9a6269a6bf4
Leo Collin <leo.collinh@gmail.com>
2026-09-21 10:40:57 +0200

    changed xdg-open to gio in yazi config

--- .config/hypr/colors.lua
+++ .config/hypr/colors.lua
@@ -1,7 +1,7 @@
 return {
-    image = "/home/leo/Pictures/Wallpapers/gloomy_landscape.jpg",
+    image = "/home/leo/Pictures/Wallpapers/sunset_field.jpg",
 
-    background = "0xff0f1416",
+    background = "0xff191114",
 
     error = "0xffffece9",
 
@@ -9,11 +9,11 @@ return {
 
     inverse_on_surface = "0xff000000",
 
-    inverse_primary = "0xff004f5f",
+    inverse_primary = "0xff6e3553",
 
-    inverse_surface = "0xffdee3e6",
+    inverse_surface = "0xffeedfe3",
 
-    on_background = "0xffdee3e6",
+    on_background = "0xffeedfe3",
 
     on_error = "0xff000000",
 
@@ -25,7 +25,7 @@ return {
 
     on_primary_fixed = "0xff000000",
 
-    on_primary_fixed_variant = "0xff001419",
+    on_primary_fixed_variant = "0xff2a0019",
 
     on_secondary = "0xff000000",
 
@@ -33,7 +33,7 @@ return {
 
     on_secondary_fixed = "0xff000000",
 
-    on_secondary_fixed_variant = "0xff001419",
+    on_secondary_fixed_variant = "0xff1e0b15",
 
     on_surface = "0xffffffff",
 
@@ -45,60 +45,60 @@ return {
 
     on_tertiary_fixed = "0xff000000",
 
-    on_tertiary_fixed_variant = "0xff090e2c",
+    on_tertiary_fixed_variant = "0xff210b00",
 
-    outline = "0xffe9f1f5",
+    outline = "0xffffebf1",
 
-    outline_variant = "0xffbbc4c7",
+    outline_variant = "0xffd0bec4",
 
-    primary = "0xffd8f5ff",
+    primary = "0xffffebf1",
 
-    primary_container = "0xff81cee4",
+    primary_container = "0xfff9acd0",
 
-    primary_fixed = "0xffafecff",
+    primary_fixed = "0xffffd8e7",
 
-    primary_fixed_dim = "0xff85d2e8",
+    primary_fixed_dim = "0xfffdb0d4",
 
     scrim = "0xff000000",
 
-    secondary = "0xffdbf4fd",
+    secondary = "0xffffebf1",
 
-    secondary_container = "0xffaec7cf",
+    secondary_container = "0xffdcb9c7",
 
-    secondary_fixed = "0xffcee7ef",
+    secondary_fixed = "0xfffdd9e7",
 
-    secondary_fixed_dim = "0xffb2cbd3",
+    secondary_fixed_dim = "0xffe0bdcb",
 
     shadow = "0xff000000",
 
-    source_color = "0xff345660",
+    source_color = "0xff6a314f",
 
-    surface = "0xff0f1416",
+    surface = "0xff191114",
 
-    surface_bright = "0xff4b5153",
+    surface_bright = "0xff574d51",
 
-    surface_container = "0xff2c3133",
+    surface_container = "0xff372e31",
 
-    surface_container_high = "0xff373c3e",
+    surface_container_high = "0xff42393c",
 
-    surface_container_highest = "0xff42484a",
+    surface_container_highest = "0xff4e4448",
 
-    surface_container_low = "0xff1b2022",
+    surface_container_low = "0xff251d21",
 
     surface_container_lowest = "0xff000000",
 
-    surface_dim = "0xff0f1416",
+    surface_dim = "0xff191114",
 
-    surface_tint = "0xff85d2e8",
+    surface_tint = "0xfffdb0d4",
 
-    surface_variant = "0xff40484b",
+    surface_variant = "0xff504348",
 
-    tertiary = "0xffefeeff",
+    tertiary = "0xffffece3",
 
-    tertiary_container = "0xffbcc0e7",
+    tertiary_container = "0xffeeb796",
 
-    tertiary_fixed = "0xffdee0ff",
+    tertiary_fixed = "0xffffdbc8",
 
-    tertiary_fixed_dim = "0xffc0c4eb",
+    tertiary_fixed_dim = "0xfff2bb99",
 
 }

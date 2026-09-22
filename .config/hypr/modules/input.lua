0cf2a0a2fedaa962a76e4b98f632769942740b7f
Leo Collin <leo.collinh@gmail.com>
2026-09-22 11:47:48 +0200

    changes

--- .config/hypr/modules/input.lua
+++ .config/hypr/modules/input.lua
@@ -3,32 +3,32 @@
 ---------------
 
 hl.config({
-  input = {
-    kb_layout = "gb, se",
-    kb_variant = "",
-    kb_model = "",
-    -- kb_options = "compose:ralt",
-    kb_rules = "",
+	input = {
+		kb_layout = "gb, se",
+		kb_variant = "",
+		kb_model = "",
+		kb_options = "compose:caps",
+		kb_rules = "",
 
-    follow_mouse = 1,
+		follow_mouse = 1,
 
-    sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+		sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
 
-    touchpad = {
-      natural_scroll = true,
-    },
-  },
+		touchpad = {
+			natural_scroll = true,
+		},
+	},
 })
 
 hl.gesture({
-  fingers = 3,
-  direction = "horizontal",
-  action = "workspace",
+	fingers = 3,
+	direction = "horizontal",
+	action = "workspace",
 })
 
 -- Example per-device config
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
 hl.device({
-  name = "epic-mouse-v1",
-  sensitivity = -0.5,
+	name = "epic-mouse-v1",
+	sensitivity = -0.5,
 })
